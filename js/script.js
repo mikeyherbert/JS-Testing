56cda37e3cbab9733e0df6aeca3d1fef74bce9df
mikeyherbert <33906535+mikeyherbert@users.noreply.github.com>
2017-12-01 18:17:13 +0000

    update

---
+++ js/script.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $(".panel p").text("Hello World! This is JQuery speaking. If you are reading this it means your script has worked and you have figured out how to use me!");
+});
