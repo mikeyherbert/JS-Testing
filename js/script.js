4ffb40586b4aff3339b7134683e404d181cadaa3
Mikey Herbert <iocaffeine13@gmail.com>
2017-12-03 20:58:06 +0000

    sdsd

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
     $(".panel p").text("Hello World! This is JQuery speaking. If you are reading this it means your script has worked and you have figured out how to use me!");
+    
+    function onHover() {
+        $(".panel").mouseenter(function () {
+            $(".panel").css("background-color: white", "color: black");
+        });
+    }
 });
